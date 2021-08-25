fc009b379ea6fb46d73214923f3079c40e603d09
xmif1 <xandru.mifsud@gmail.com>
2021-08-25 16:25:33 +0200

    Began adding xgboost trainer benchmark

--- bdt/BoostedDTBenchmarks.cxx
+++ bdt/BoostedDTBenchmarks.cxx
@@ -31,7 +31,7 @@ static void BM_TMVA_BDTTraining(benchmark::State &state){
    double mem_res = 0.0;
 
    // Open output file
-   TString outfileName( "bdt_bench_train_output.root" );
+   TString outfileName( "bdt_tmva_bench_train_output.root" );
    TFile* outputFile = TFile::Open(outfileName, "RECREATE");
 
    // Set up (generate one extra event for testing)
@@ -39,7 +39,7 @@ static void BM_TMVA_BDTTraining(benchmark::State &state){
    TTree *bkgTree = genTree("bkgTree", nEvents, nVars,-0.3, 0.5, 101);
 
    // Prepare a DataLoader instance, registering the signal and background TTrees
-   auto *dataloader = new TMVA::DataLoader("bdt-bench");
+   auto *dataloader = new TMVA::DataLoader("bdt_tmva_bench");
    dataloader->AddSignalTree(sigTree);
    dataloader->AddBackgroundTree(bkgTree);
 
@@ -51,7 +51,6 @@ static void BM_TMVA_BDTTraining(benchmark::State &state){
       dataloader->AddVariable(var_name.c_str(), 'D');
    }
 
-   // For each benchmark we specifically ignore this test event such that we exclusively benchmark training.
    dataloader->PrepareTrainingAndTestTree("",
                   Form("SplitMode=Block:nTrain_Signal=%i:nTrain_Background=%i:!V", nEvents, nEvents));
 
@@ -61,7 +60,7 @@ static void BM_TMVA_BDTTraining(benchmark::State &state){
       ROOT::EnableImplicitMT(state.range(2));
 
       // Create factory instance
-      auto factory = new TMVA::Factory("bdt-bench", outputFile,
+      auto factory = new TMVA::Factory("bdt_tmva_bench", outputFile,
                                     "Silent:!DrawProgressBar:AnalysisType=Classification");
 
       // Get current memory usage statistics after setup
@@ -114,6 +113,84 @@ static void BM_TMVA_BDTTraining(benchmark::State &state){
 }
 BENCHMARK(BM_TMVA_BDTTraining)->ArgsProduct({{2000, 1000, 400, 100}, {10, 8, 6, 4, 2}, {1, 4, 8, 16}});
 
+static void BM_XGBOOST_BDTTraining(benchmark::State &state){
+   // Parameters
+   UInt_t nVars = 4;
+   UInt_t nEvents = 500;
+   Bool_t mem_stats = (state.range(0) == 2000) && (state.range(1) == 10);
+
+   // Memory benchmark data placeholder
+   ProcInfo_t pinfo;
+   Long_t init_mem_res, term_mem_res; init_mem_res = term_mem_res = 0;
+   double mem_res = 0.0;
+
+   // Open output file
+   TString outfileName( "bdt_xgb_bench_train_output.root" );
+   TFile* outputFile = TFile::Open(outfileName, "RECREATE");
+
+   // Set up (generate one extra event for testing)
+   TTree *sigTree = genTree("sigTree", nEvents, nVars,0.3, 0.5, 100);
+   TTree *bkgTree = genTree("bkgTree", nEvents, nVars,-0.3, 0.5, 101);
+
+   // Prepare a DataLoader instance, registering the signal and background TTrees
+   auto *dataloader = new TMVA::DataLoader("bdt_xgb_bench");
+   dataloader->AddSignalTree(sigTree);
+   dataloader->AddBackgroundTree(bkgTree);
+
+   // Register variables in dataloader, using naming convention for randomly generated TTrees in MakeRandomTTree.h
+   for(UInt_t i = 0; i < nVars; i++){
+      string var_name = "var" + to_string(i);
+      string var_leaflist = var_name + "/F";
+
+      dataloader->AddVariable(var_name.c_str(), 'D');
+   }
+
+   dataloader->PrepareTrainingAndTestTree("",
+                  Form("SplitMode=Block:nTrain_Signal=%i:nTrain_Background=%i:!V", nEvents, nEvents));
+
+   // Extract the training data set and convert it to an XGBoost readable format
+   xgboost_data* xg_train_data = ROOTToXGBoost(dataloader->GetDefaultDataSetInfo(), TMVA::Types::kTraining);
+
+   // Benchmarking
+   UInt_t iter_c = 0;
+   for(auto _: state){
+      // Set the options for the BoosterHandle instance that will be trained...
+      xgbooster_opts opts;
+      opts.push_back(kv_pair("max_depth", std::to_string((int) state.range(1))));
+
+      // Get current memory usage statistics after setup
+      if(mem_stats && iter_c == 0){
+         gSystem->GetProcInfo(&pinfo);
+         init_mem_res = pinfo.fMemResident;
+      }
+
+      // Call the simple trainer function we implemented based on XGBoost's C-api...
+      BoosterHandle xgbooster = xgboost_train(xg_train_data, &opts, state.range(0));
+
+      // Maintain Memory statistics (independent from Google Benchmark)
+      if(mem_stats && iter_c == 0){
+         gSystem->GetProcInfo(&pinfo);
+         term_mem_res = pinfo.fMemResident;
+         mem_res += (double) (term_mem_res - init_mem_res);
+      }
+
+      iter_c++;
+   }
+
+   if(mem_stats){
+      mem_res *= iter_c;
+      state.counters["Resident Memory"] = benchmark::Counter(mem_res, benchmark::Counter::kAvgIterations);
+   }
+
+   // Teardown
+   delete sigTree;
+   delete bkgTree;
+
+   outputFile->Close();
+   delete outputFile;
+}
+BENCHMARK(BM_TMVA_BDTTraining)->ArgsProduct({{2000, 1000, 400, 100}, {10, 8, 6, 4, 2}});
+
 static void BM_TMVA_BDTTesting(benchmark::State &state){
    // Parameters
    UInt_t nVars = 4;
@@ -130,14 +207,14 @@ static void BM_TMVA_BDTTesting(benchmark::State &state){
    TFile* outputFile = TFile::Open(outfileName, "RECREATE");
 
    // Set up
-   auto inputFile = new TFile("bdt_bench_test_input.root","RECREATE");
+   auto inputFile = new TFile("bdt_tmva_bench_test_input.root","RECREATE");
    TTree *testTree = genTree("testTree", nEvents, nVars,0.3, 0.5, 102, false);
    testTree->Write();
    delete testTree;
    inputFile->Close();
    delete inputFile;
 
-   ROOT::RDataFrame testDF("testTree","bdt_bench_test_input.root");
+   ROOT::RDataFrame testDF("testTree","bdt_tmva_bench_test_input.root");
    auto testTensor = AsTensor<Float_t>(testDF);
 
    // Benchmarking
@@ -154,7 +231,7 @@ static void BM_TMVA_BDTTesting(benchmark::State &state){
          init_mem_res = pinfo.fMemResident;
       }
 
-      RReader model("./bdt-bench/weights/bdt-bench_BDT_" + key + ".weights.xml");
+      RReader model("./bdt_tmva_bench/weights/bdt_tmva_bench_BDT_" + key + ".weights.xml");
       model.Compute(testTensor);
 
       // Maintain Memory statistics (independent from Google Benchmark)
